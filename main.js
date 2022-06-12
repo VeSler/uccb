ade6efa87a5660f186868264e61b8f5806c0272c
VeSler <shmorgun.vitaliy@gmail.com>
2022-06-12 15:37:07 +0200

    v0.1.22

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * 0.1.21
+ * v0.1.22
  * 
  * 0.1.0 - новая структура интерфейса
  * 
@@ -78,6 +78,103 @@ module.exports = class Uccb extends EventEmitter {
         };
     }
 
+
+    /**
+     * PUBLIC
+     */
+
+    /**
+     * @brief Start device
+     * @public
+     * @param {*} mode = 'O' | 'L' | 'l'
+     * @returns Promise
+     */
+    async start(mode) {
+        return new Promise((resolve, reject) => {
+            let _mode = mode ?? "O";
+            if (!["O", "L", "l"].includes(_mode)){
+                reject(new Error(`Can't Start device with unknown parameter. Mode: ${_mode}`))
+            }
+            try{
+                await this.prepareConnection();
+                /*
+                await this.portOpen();
+                await this.getDeviceInfo();
+                await this.canOpen(_mode);
+                */
+                this.emit('canStart', 'CANBUS started successfully');
+                resolve();
+            }catch(e){
+                reject(e);
+            }    
+        })
+    }
+
+    /**
+     * @brief Stop device
+     * @public
+     * @returns Promise
+     */
+     async stop() {
+        return new Promise((resolve, reject) => {
+            try{
+                /*
+                await this.canClose();
+                await this.portClose();
+                */
+                this.emit('canStop', 'CANBUS stopped successfully');
+                resolve();
+            }catch(e){
+                reject(e);
+            }
+        })
+    }
+
+
+    /**
+     * @brief 
+     */
+     async prepareConnection() {
+        try{           
+            let list = await this.getUARTList();
+            for(let item of list){
+                if (item?.pnpId.includes("CAN_USB_ConverterBasic")) {
+                    this.portName = item?.path;
+                    return;
+                }
+            }
+            throw new Error(`Path not found. List devices: ${JSON.stringify(res)}`)
+        }catch(e){
+            throw e;
+        }
+    }
+
+
+
+    /**
+     * PRIVATE
+     */
+
+
+    /**
+     * @private ?
+     * @brief Return list USB-devices in system
+     */
+
+     async getUARTList() {
+        try {
+            return await SerialPort.list();
+        } catch (e){
+            throw e;
+        }
+    }
+
+
+    /**
+     * @private
+     * @param {*} br 
+     * @returns TRUE or FALSE 
+     */
     checkBaudRate(br) {
         return this.baudRate.includes(br);
     }
